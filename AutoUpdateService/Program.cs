d9ed9eeaf5923e1256a3188b538a70b4b8c35646
greendev3712 <businessxfreeman@gmail.com>
2023-01-11 01:56:47 +0100

    test

--- AutoUpdateService/Program.cs
+++ AutoUpdateService/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,8 +13,13 @@ namespace AutoUpdateService
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            // setup the auto update, and exit if there was an update
+            Updater.GitHubRepo = @"/greendev3712/AutoUpdateService";
+            if (Updater.AutoUpdate(args))
+                return;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
---
+++ AutoUpdateService/Updater.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace AutoUpdateService
+{
+    static class Updater
+    {
+        static readonly Lazy<IDictionary<Version, Uri>> _lazyVersionUrls =
+            new Lazy<IDictionary<Version, Uri>>(() => GetVersionUrls());
+        private static IDictionary<Version, Uri> _versionUrls
+        {
+            get
+            {
+                return _lazyVersionUrls.Value;
+            }
+        }
+
+        public static string GitHubRepo { get; set; }
+        public static bool HasUpdate { 
+            get 
+            {
+                var v = Assembly.GetEntryAssembly().GetName().Version;
+                foreach (var e in _versionUrls)
+                {
+                    if (e.Key > v) return true;
+                }
+                return false;
+            }
+        }
+
+        public static bool AutoUpdate(string[] args)
+        {
+            if (HasUpdate)
+            {
+                Update(args);
+                return true;
+            }
+            return false;
+        }
+
+        private static void Update(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        static IDictionary<Version, Uri> GetVersionUrls()
+        {
+            string pattern = string.Concat(
+                Regex.Escape(GitHubRepo),
+                @"\/releases\/download\/Update.v[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+.*\.zip");
+
+            Regex urlMatcher = new Regex(pattern, RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            var result = new Dictionary<Version, Uri>();
+            WebRequest wrq = WebRequest.Create(string.Concat(@"https://github.com", GitHubRepo, "/releases/latest"));
+            WebResponse wrs;
+
+            try
+            {
+                wrs = wrq.GetResponse();
+            } catch(WebException ex)
+            {
+                Debug.WriteLine("Error fetching repo: " + ex.Message);
+                return result;
+            }
+            using (var sr = new StreamReader(wrs.GetResponseStream()))
+            {
+                string line;
+                while((line = sr.ReadLine()) != null)
+                {
+                    var match = urlMatcher.Match(line);
+                    if (match.Success)
+                    {
+                        var uri = new Uri(string.Concat(@"http://github.com", match.Value));
+                        var vs = match.Value.LastIndexOf("/Update.v");
+                        var sa = match.Value.Substring(vs + 10).Split('.', '/');
+                        var v = new Version(int.Parse(sa[0]), int.Parse(sa[1]), int.Parse(sa[2]), int.Parse(sa[3]));
+                        result.Add(v, uri);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
